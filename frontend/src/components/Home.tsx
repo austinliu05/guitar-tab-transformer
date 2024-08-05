b6f5b71a62270ca7ab23e16b5f3841d93585dcd7
Austin Liu <austin.liu05@yahoo.com>
2024-08-05 01:25:38 -0500

    added text to the homepage

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -4,10 +4,25 @@ import { Container, Row, Col } from 'react-bootstrap';
 const Home: React.FC = () => {
     return (
         <Container>
-            <Row className="my-5">
+            <Row className="mt-5 text-center">
+                <h1>Welcome to Guitar Tab Transformer!</h1>
+            </Row>
+            <Row className='mt-5'>
+                <h2>About</h2>
+                <p className='mt-4'>The final goal of this project is to create a Recurrent Neural Network capable of transforming any song (.mp3 file) into <a
+                    href='https://www.perplexity.ai/search/what-are-guitar-tabs-pJADgdOlTz6KabGjnNobOg#0'
+                    target="_blank"
+                    rel="noopener noreferrer">guitar tabs</a>.
+                    As my first machine learning project, I have decided to divide the project into incremental steps as shown below.</p>
+            </Row>
+            <Row className='mt-5'>
                 <Col>
-                    <h1>Welcome to Guitar Tab Transformer</h1>
-                    <p>This is a placeholder homepage.</p>
+                    <h2>Timeline</h2>
+                </Col>
+            </Row>
+            <Row className='my-5'>
+                <Col>
+                    <h2>Frustration</h2>
                 </Col>
             </Row>
         </Container>
