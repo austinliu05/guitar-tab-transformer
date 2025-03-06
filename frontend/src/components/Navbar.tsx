b521255c8581ab2f72071105c6b5caf3be6dd679
Austin Liu <austin.liu05@yahoo.com>
2025-03-06 02:41:22 -0500

    Updated landing page with latest updates

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../styles/CustomNavbar.scss';
 import logo from '../assets/images/logo.png';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 
 const CustomNavbar: React.FC = () => {
     const location = useLocation();
@@ -11,8 +11,13 @@ const CustomNavbar: React.FC = () => {
         <Navbar bg="dark" variant='dark' expand="lg">
             <Container>
                 <Navbar.Brand>
-                    <img src={logo} width='50' height='50' className="d-inline-block align-top rotated-logo"
-                        alt="Guitar Tab Transformer logo" />
+                    <img 
+                        src={logo} 
+                        width='50' 
+                        height='50' 
+                        className="d-inline-block align-top rotated-logo"
+                        alt="Guitar Tab Transformer logo" 
+                    />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -24,37 +29,32 @@ const CustomNavbar: React.FC = () => {
                         >
                             Home
                         </Nav.Link>
-                        <Nav.Link 
-                            target="_blank" 
-                            href='https://www.kaggle.com/code/apoxieforest/gtt-intro-preprocessing'
-                        >
-                            Preprocessing
-                        </Nav.Link>
-                        <Nav.Link 
-                            target="_blank" 
-                            href='https://www.kaggle.com/code/apoxieforest/gtt-binary-classification'
-                        >
-                            Binary Classification
-                        </Nav.Link>
-                        <Nav.Link 
-                            target="_blank" 
-                            href='https://www.kaggle.com/code/apoxieforest/gtt-multiclass-classification'
-                        >
-                            Multiclass Classification
-                        </Nav.Link>
-                        <Nav.Link 
-                            target="_blank" 
-                            href='https://www.kaggle.com/code/apoxieforest/gtt-note-identification'
-                        >
-                            Notehead Classification
-                        </Nav.Link>
-                        <Nav.Link
-                            as={Link}
-                            to="/blog/notehead-classification"
-                            className={location.pathname === '/blog/notehead-classification' ? "active-link" : ""}
-                        >
-                            Notehead Classification
-                        </Nav.Link>
+                        <NavDropdown title="Previous Works" id="previous-works-dropdown">
+                            <NavDropdown.Item 
+                                target="_blank" 
+                                href='https://www.kaggle.com/code/apoxieforest/gtt-intro-preprocessing'
+                            >
+                                Preprocessing
+                            </NavDropdown.Item>
+                            <NavDropdown.Item 
+                                target="_blank" 
+                                href='https://www.kaggle.com/code/apoxieforest/gtt-binary-classification'
+                            >
+                                Binary Classification
+                            </NavDropdown.Item>
+                            <NavDropdown.Item 
+                                target="_blank" 
+                                href='https://www.kaggle.com/code/apoxieforest/gtt-multiclass-classification'
+                            >
+                                Multiclass Classification
+                            </NavDropdown.Item>
+                            <NavDropdown.Item 
+                                target="_blank" 
+                                href='https://www.kaggle.com/code/apoxieforest/gtt-note-identification'
+                            >
+                                Notehead Classification
+                            </NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- frontend/src/components/Timeline.tsx
+++ frontend/src/components/Timeline.tsx
@@ -8,10 +8,16 @@ interface TimelineItemProps {
     image: string;
 }
 const timelineData = [
+    { 
+        date: "03/2025", 
+        title: "Starting from Square 1", 
+        description: "After a brief hiatus following Summer 2024, I re-engaged with my passion project when Claude reached out in November 2024 with a similar idea and proposed a collaboration. Claude is currently a researcher specializing in NLP at the University of Virginia. Together, we have built a small database of guitar tabs and encoded them using dataGP files. In process of developing and building the model before tuning it to our needs.", 
+        image: "" 
+    },
     {
-        date: "06/2024",
-        title: "Building a Foundation",
-        description: "I focused on deepening my understanding of Machine Learning by completing courses from MIT OpenCourseWare. This intensive study provided a solid foundation for my upcoming projects.\n Resources:",
+        date: "08/2024",
+        title: "Advancing to Notehead Classification",
+        description: "Currently working on creating a model to recognize noteheads and their relative positions in the staff line. Once I have completed this task, I can begin designing an algorithm to convert the processed music sheets into guitar tabs.",
         image: ""
     },
     {
@@ -21,18 +27,18 @@ const timelineData = [
         image: ""
     },
     {
-        date: "08/2024",
-        title: "Advancing to Notehead Classification",
-        description: "Currently working on creating a model to recognize noteheads and their relative positions in the staff line. Once I have completed this task, I can begin designing an algorithm to convert the processed music sheets into guitar tabs.",
+        date: "06/2024",
+        title: "Building a Foundation",
+        description: "I focused on deepening my understanding of Machine Learning by completing courses from MIT OpenCourseWare. This intensive study provided a solid foundation for my upcoming projects.\n Resources:",
         image: ""
-    },    
+    },
 ];
 
-const TimelineItem: React.FC<TimelineItemProps> = ({date, title, description}) => {
+const TimelineItem: React.FC<TimelineItemProps> = ({ date, title, description }) => {
     return (
         <div className='timeline-item mb-5'>
-            <div className='d-flex flex-row align-items-center'> 
-                <div className = 'timeline-date me-3'>
+            <div className='d-flex flex-row align-items-center'>
+                <div className='timeline-date me-3'>
                     <h5 className='mb-0'>{date}</h5>
                 </div>
                 <div className='timeline-content p-3 rounded'>
@@ -48,11 +54,11 @@ const Timeline: React.FC = () => {
         <div className='timeline'>
             {timelineData.map((item, index) => (
                 <TimelineItem
-                key={index}
-                date={item.date}
-                title={item.title}
-                description={item.description}
-                image={item.image}
+                    key={index}
+                    date={item.date}
+                    title={item.title}
+                    description={item.description}
+                    image={item.image}
                 />
             ))}
         </div>
